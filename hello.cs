ded7245e9e062253ce79007befeb79c870c3c539
Peter Ndungu <93506710+petreleven@users.noreply.github.com>
2025-07-04 21:03:01 +0300

    Update hello.cs

--- hello.cs
+++ hello.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 float score = 100.47f;
 string country = "Kenya";
@@ -13,7 +14,7 @@ List<string>cities = new List<string>() {"nairobi", "paris", "johannesburg"};
 List<int> mynumbers = new List<int>(){122,137,658,19};
 List<float> anotherlist = new List<float>(){458.337f, 747.2747f, 2847.773f};
 
-Console.WriteLine(cities[0])
+Console.WriteLine(cities[0]);
 
 
 
