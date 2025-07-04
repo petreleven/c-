7b03f7374cff2a8fca103d2e371b288664b39556
Peter Ndungu <93506710+petreleven@users.noreply.github.com>
2025-07-04 21:00:35 +0300

    Create hello.cs

---
+++ hello.cs
@@ -0,0 +1,21 @@
+using System;
+
+float score = 100.47f;
+string country = "Kenya";
+bool isalive = true;
+
+Console.WriteLine(country);
+Console.WriteLine(1000 + 99);
+Console.WriteLine(113662 - 99);
+Console.WriteLine(113662 * 99);
+
+List<string>cities = new List<string>() {"nairobi", "paris", "johannesburg"};
+List<int> mynumbers = new List<int>(){122,137,658,19};
+List<float> anotherlist = new List<float>(){458.337f, 747.2747f, 2847.773f};
+
+Console.WriteLine(cities[0])
+
+
+
+
+
